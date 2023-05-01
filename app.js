e61cdb4e14aca4d92eebe35b455a35a7ac939031
nick <chenhsuanfu@gmail.com>
2023-05-01 16:01:52 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
 
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose') // 載入 mongoose
 const RL = require('./models/RL')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const routes = require('./routes')
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production'){
     require('dotenv').config()
 }
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) 
-const db = mongoose.connection // 取得資料庫連線狀態
-// 連線異常
-db.on('error', () => {
-    console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-    console.log('mongodb connected!')
-})
+
+const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 const port = 3000
--- models/seeds/RLSeeder.js
+++ models/seeds/RLSeeder.js
@@ -1,19 +1,12 @@
-const mongoose = require('mongoose')
-const RL = require('../RL')
-const restaurant_list = require('../../restaurant.json').results
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production'){
     require('dotenv').config()
 }
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) 
+const RL = require('../RL')
+const restaurant_list = require('../../restaurant.json').results
+const db = require('../../config/mongoose')
 
-const db = mongoose.connection // 取得資料庫連線狀態
-// 連線異常
-db.on('error', () => {
-    console.log('mongodb error!')
-})
-// 連線成功
 db.once('open', () => {
     console.log('mongodb connected!')
     RL.create(restaurant_list)
