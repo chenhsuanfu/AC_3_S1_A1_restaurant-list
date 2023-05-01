cd5f2bd20007b5161f2f58476bc830708acd9877
nick <chenhsuanfu@gmail.com>
2023-05-01 15:25:52 +0800

    feat: add method-override

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 
 const express = require('express')
 const exphbs = require('express-handlebars')
-
 const mongoose = require('mongoose') // 載入 mongoose
 const RL = require('./models/RL')
-
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production'){
@@ -29,6 +28,7 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 // 瀏覽全部的餐廳
 app.get('/', (req, res)=>{  
